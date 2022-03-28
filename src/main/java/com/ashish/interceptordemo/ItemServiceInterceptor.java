d89a0ed3fbb3f1c1340d966ea66c5f8aa39f9a64
Ashish Sharda <ashishjsharda@gmail.com>
2022-03-28 05:54:19 -0500

    Add postHandle

--- src/main/java/com/ashish/interceptordemo/ItemServiceInterceptor.java
+++ src/main/java/com/ashish/interceptordemo/ItemServiceInterceptor.java
@@ -1,6 +1,7 @@
 package com.ashish.interceptordemo;
 
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,4 +13,9 @@ public class ItemServiceInterceptor implements HandlerInterceptor {
 
         return true;
     }
+
+    @Override
+    public void postHandle(
+            HttpServletRequest request, HttpServletResponse response, Object handler,
+            ModelAndView modelAndView) throws Exception {}
 }
