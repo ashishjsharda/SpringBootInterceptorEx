dcabdf122747ced6655a7f89fbdf36b4aef2e43d
Ashish Sharda <ashishjsharda@gmail.com>
2022-03-28 05:57:16 -0500

    Add postHandle

--- src/main/java/com/ashish/interceptordemo/ItemServiceInterceptor.java
+++ src/main/java/com/ashish/interceptordemo/ItemServiceInterceptor.java
@@ -18,10 +18,14 @@ public class ItemServiceInterceptor implements HandlerInterceptor {
     @Override
     public void postHandle(
             HttpServletRequest request, HttpServletResponse response, Object handler,
-            ModelAndView modelAndView) throws Exception {}
+            ModelAndView modelAndView) throws Exception {
+        System.out.println("Post Handle method is Called");
+    }
 
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response,
-                                Object handler, Exception exception) throws Exception {}
+                                Object handler, Exception exception) throws Exception {
+        System.out.println("After Completion method is completed");
+    }
 }
