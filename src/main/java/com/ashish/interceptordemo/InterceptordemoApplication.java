90c68f1f335da5ced87a02a9dba5703e6725ba37
Ashish Sharda <ashishjsharda@gmail.com>
2022-03-28 05:50:49 -0500

    first commit

---
+++ src/main/java/com/ashish/interceptordemo/InterceptordemoApplication.java
@@ -0,0 +1,13 @@
+package com.ashish.interceptordemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterceptordemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterceptordemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ashish/interceptordemo/InterceptordemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashish.interceptordemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterceptordemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
