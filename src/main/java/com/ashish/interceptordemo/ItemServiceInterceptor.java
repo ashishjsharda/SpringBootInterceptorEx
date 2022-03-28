e7d08bec149c98b7ef5cf81366e50446c502be41
Ashish Sharda <ashishjsharda@gmail.com>
2022-03-28 05:53:37 -0500

    Update Interceptor methods

--- src/main/java/com/ashish/interceptordemo/ItemServiceInterceptor.java
+++ src/main/java/com/ashish/interceptordemo/ItemServiceInterceptor.java
@@ -2,5 +2,14 @@ package com.ashish.interceptordemo;
 
 import org.springframework.web.servlet.HandlerInterceptor;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 public class ItemServiceInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(
+            HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        return true;
+    }
 }
